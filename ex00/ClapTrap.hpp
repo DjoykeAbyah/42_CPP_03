66f50419b211c4895e663ac66e4f2e41e045a231
DjoykeAbyah <crudendjoyke@gmail.com>
2024-06-12 13:38:41 +0200

    added orthodox canonical form

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/11 21:26:59 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/06/11 22:24:49 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/06/12 13:37:48 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,15 @@ class ClapTrap
 		int _energyPoints = 10;
 		int _attackDamage = 0;
 		
-		ClapTrap(std::string& Name);
-		~ClapTrap();
-
-		//orthodox canonical form
-		// Fixed(void);//default constructor
-		// Fixed(const Fixed& other); //copy constructor
-		// Fixed& operator=(const Fixed& other); //copy assignment operator overload
-		// ~Fixed();
 		
 	public:
+		ClapTrap();//default constructor
+		ClapTrap(const std::string& Name);//constructor with parameters
+		ClapTrap(const ClapTrap& other);//copy constructor
+		const ClapTrap& operator=(const ClapTrap& other); //copy assignment operator overload
+		~ClapTrap();
+		
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
 };
